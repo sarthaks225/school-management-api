7ab37d18ce7ea1be86f408cf96e921022efd11a0
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2025-03-07 22:53:29 +0530

    push

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,19 @@
+require("dotenv").config();
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "9893",
-  database: "tmdbuser",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306, // Default MySQL port
 });
 
 db.connect((err) => {
-  if (err) throw err;
+  if (err) {
+    console.error("Database connection failed:", err);
+    return;
+  }
   console.log("MySQL Connected successfully...");
 });
 
