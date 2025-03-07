84a23326da8ce4ee7fbc6aa2e09bc2860823331d
Sarthak Sharma <96478734+sarthaks225@users.noreply.github.com>
2025-03-07 23:57:18 +0530

    adding env functionality

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,21 @@
-require("dotenv").config();
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306, // Default MySQL port
+  port: process.env.DB_PORT || 3306,
 });
 
 db.connect((err) => {
   if (err) {
-    console.error("Database connection failed:", err);
+    console.error("Database connection failed:", err.code, err.message);
     return;
   }
   console.log("MySQL Connected successfully...");
